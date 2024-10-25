05ad28f08378ee293887ace99863d231aa7ee3ba
Mike H <mikeh09@gmail.com>
2024-10-25 14:54:27 -0700

    more basic setup cleanup

--- next.config.js
+++ next.config.js
@@ -1,1 +1,37 @@
-module.exports = {}
+module.exports = {
+  swcMinify: true,
+  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  onDemandEntries: {
+    // period (in ms) where the server will keep pages in the buffer
+    maxInactiveAge: 120 * 1000,
+    // number of pages that should be kept simultaneously without being disposed
+    pagesBufferLength: 3,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          // Prevents the page from being loaded in an iframe (clickjacking prevention)
+          { key: 'Content-Security-Policy', value: `frame-ancestors 'none';` },
+        ],
+      },
+    ]
+  },
+  webpack: (config) => {
+    // https://github.com/rainbow-me/rainbowkit/blob/main/examples/with-next-app/next.config.js
+    // https://github.com/WalletConnect/walletconnect-monorepo/issues/1908
+
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  },
+}
