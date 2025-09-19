a8df0bf976ccc5da5b7ed155b2a372bf08908a45
xh3b4sd <xh3b4sd@gmail.com>
2025-09-19 12:46:50 +0200

    red

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-sky-300">
+      <body className="bg-red-300">
         <App>{children}</App>
       </body>
     </html>
