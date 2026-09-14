31747a4a24ebf9ad7f6baabc7b5cb13706d7ba83
Mike <mihoward21@users.noreply.github.com>
2026-09-14 15:54:01 -0700

    Add Robinhood Chain support to Splits Lite (#162)

--- src/constants/erc20.ts
+++ src/constants/erc20.ts
@@ -14,6 +14,7 @@ import {
   celo,
   tempo,
   tempoModerato,
+  robinhood,
 } from 'viem/chains'
 
 export const ERC_20_TOKEN_LIST_BY_CHAIN: { [key: number]: string[] } = {
@@ -87,4 +88,8 @@ export const ERC_20_TOKEN_LIST_BY_CHAIN: { [key: number]: string[] } = {
     '0x20c0000000000000000000000000000000000003',
   ],
   [hoodi.id]: [],
+  [robinhood.id]: [
+    '0x5fc5360D0400a0Fd4f2af552ADD042D716F1d168', // USDG
+    '0x0Bd7D308f8E1639FAb988df18A8011f41EAcAD73', // WETH
+  ],
 }
